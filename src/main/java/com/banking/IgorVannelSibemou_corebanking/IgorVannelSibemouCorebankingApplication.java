7d148a67260159ecfbdf3cbf66dc1c1f68219878
igorvannel <sibpro61@gmail.com>
2025-03-27 15:22:39 +0100

    Initialisation du projet Core Banking

---
+++ src/main/java/com/banking/IgorVannelSibemou_corebanking/IgorVannelSibemouCorebankingApplication.java
@@ -0,0 +1,13 @@
+package com.banking.IgorVannelSibemou_corebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgorVannelSibemouCorebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgorVannelSibemouCorebankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/IgorVannelSibemou_corebanking/IgorVannelSibemouCorebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.IgorVannelSibemou_corebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgorVannelSibemouCorebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
